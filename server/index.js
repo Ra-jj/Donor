e3926460dbdf6b18ec07d85c70ee32c2462d2da2
Monodip Bhattacharya <monodipkolkata@gmail.com>
2026-08-12 01:20:33 +0530

    Fix Express 5 wildcard routing error for Render deploy

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
